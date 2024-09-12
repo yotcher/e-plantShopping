78a77b162cae3423a83fd333cb677e0e87bafe59
yot <yot@example.com>
2024-09-12 10:08:42 -0400

    added fuctionality

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -8,6 +8,7 @@ function ProductList() {
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
     const dispatch = useDispatch()
+    const cart = useSelector(state => state.cart.items);
     const plantsArray = [
         {
             category: "Air Purifying Plants",
